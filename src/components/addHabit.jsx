aa09b99e8fc0d05fdd81b11cef04350eb7a1432b
kangmuni <kangmoonee@gmail.com>
2021-11-25 14:31:56 +0900

    Change addHabit component for functional component

--- src/components/addHabit.jsx
+++ src/components/addHabit.jsx
@@ -1,32 +1,56 @@
-import React, { PureComponent } from 'react';
+import React, { memo } from 'react';
 
-class AddHabit extends PureComponent {
-  formRef = React.createRef();
-  inputRef = React.createRef();
+const AddHabit = memo((props) => {
+  const formRef = React.createRef();
+  const inputRef = React.createRef();
 
-  onSubmit = (event) => {
+  const onSubmit = (event) => {
     event.preventDefault();
-    const name = this.inputRef.current.value;
-    name && this.props.onAdd(name);
-    this.formRef.current.reset();
+    const name = inputRef.current.value;
+    name && props.onAdd(name);
+    formRef.current.reset();
   };
 
-  render() {
-    console.log('addHabit');
-    return (
-      <form ref={this.formRef} className="add-form" onSubmit={this.onSubmit}>
-        <input
-          ref={this.inputRef}
-          type="text"
-          className="add-input"
-          placeholder="Habit"
-        />
-        <button className="add-button">
-          <i className="fas fa-plus-circle"></i>
-        </button>
-      </form>
-    );
-  }
-}
+  return (
+    <form ref={formRef} className="add-form" onSubmit={onSubmit}>
+      <input
+        ref={inputRef}
+        type="text"
+        className="add-input"
+        placeholder="Habit"
+      />
+      <button className="add-button">Add</button>
+    </form>
+  );
+});
+
+// class AddHabit extends PureComponent {
+//   formRef = React.createRef();
+//   inputRef = React.createRef();
+
+//   onSubmit = (event) => {
+//     event.preventDefault();
+//     const name = this.inputRef.current.value;
+//     name && this.props.onAdd(name);
+//     this.formRef.current.reset();
+//   };
+
+//   render() {
+//     console.log('addHabit');
+//     return (
+//       <form ref={this.formRef} className="add-form" onSubmit={this.onSubmit}>
+//         <input
+//           ref={this.inputRef}
+//           type="text"
+//           className="add-input"
+//           placeholder="Habit"
+//         />
+//         <button className="add-button">
+//           <i className="fas fa-plus-circle"></i>
+//         </button>
+//       </form>
+//     );
+//   }
+// }
 
 export default AddHabit;
